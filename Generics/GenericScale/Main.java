57d1971a510917154b6bf0d1de5414f8dd3d29a8
Antonio Grigorov <grigorov.antonio96@gmail.com>
2021-08-17 16:54:07 -0300

    Added Generics Ex

---
+++ Generics/GenericScale/Main.java
@@ -0,0 +1,16 @@
+package src.Generics.GenericScale;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scale<Integer> stringScale=new Scale<>(5,10);
+        System.out.println(stringScale.getHeavier());
+
+
+
+
+
+    }
+
+}
---
+++ Generics/GenericScale/Scale.java
@@ -0,0 +1,25 @@
+package src.Generics.GenericScale;
+
+public class Scale<T extends Comparable> {
+
+        private T left;
+        private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier(){
+
+        if(this.left.equals(this.right)){
+            return null;
+        }
+        if(left.compareTo(right)>0){
+            return left;
+        }
+        return right;
+
+
+    }
+}
