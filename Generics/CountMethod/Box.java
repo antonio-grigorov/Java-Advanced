ed819fd81d62fa77429cb0642665cb25311bf129
Antonio Grigorov <grigorov.antonio96@gmail.com>
2021-08-18 17:05:06 -0300

    Added Generics Ex.

---
+++ Generics/CountMethod/Box.java
@@ -0,0 +1,33 @@
+package src.Generics.CountMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Comparable<T>> {
+
+    private List<T> elements;
+
+    public Box() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        elements.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T element : elements) {
+            sb.append(String.format("%s: %s", element.getClass().getName(), element));
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+    public int countGreaterThan(T element){
+
+        return (int)this.elements.stream()
+                .filter(e->e.compareTo(element)>0)
+                .count();
+    }
+}
