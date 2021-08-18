1fc4fcff8385db4dc56994401c141b5962d7d834
Antonio Grigorov <grigorov.antonio96@gmail.com>
2021-08-18 17:12:18 -0300

    Added Generics Ex.

---
+++ Generics/SwapMethodString/Main.java
@@ -0,0 +1,37 @@
+package src.Generics.SwapMethodString;
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner= new Scanner(System.in);
+
+
+        Box<String> box=new Box<>();
+
+        int n=Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String text = scanner.nextLine();
+            box.add(text);
+
+        }
+            String command=scanner.nextLine();
+            int swapIndex1=Integer.parseInt(command.split("\\s+")[0]);
+            int swapIndex2=Integer.parseInt(command.split("\\s+")[1]);
+
+
+            box.swap(swapIndex1,swapIndex2);
+
+
+        System.out.println(box);
+
+
+    }
+
+
+
+}
