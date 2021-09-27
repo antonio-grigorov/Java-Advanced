679b36d8c6f4c573b31949f9c4feabf57a134547
Antonio Grigorov <grigorov.antonio96@gmail.com>
2021-09-27 18:47:36 -0300

    Exercise

---
+++ Encapsulation/PizzaCreator/Pizza.java
@@ -0,0 +1,49 @@
+package src.OOP.Encapsulation.PizzaCreator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+    public Pizza(String name, int toppingsCount) {
+        this.setName(name);
+        this.setToppings(toppingsCount);
+    }
+
+    private void setToppings(int toppingsCount) {
+        if (toppingsCount < 0 || toppingsCount > 10) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+        this.toppings = new ArrayList<>(toppingsCount);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty() || name.trim().length() > 15) {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+        this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTopping(Topping topping) {
+        this.toppings.add(topping);
+    }
+
+    public double getOverralCalories() {
+        return this.dough.calculateCalories() +
+                this.toppings.stream()
+                .mapToDouble(Topping::calculateCalories)
+                .sum();
+    }
+
+}
